6893d8880e2c5067b24214045cea7a44c37df8f3
ruben <ruben.letterbox@gmail.com>
2024-06-12 19:32:17 +0400

    develop: remove toast from route middleware

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -2,7 +2,6 @@ import { useApi } from '@spaps/api';
 import { asyncGlobalSpinner } from '../packages/core/loading-worker';
 import { useUserStore } from '../stores/user';
 import { NonLoggedInRoutes, ProtectedRoute } from '../packages/routes';
-import { useToast } from 'vue-toastification';
 import { ERole } from '../packages/core/enums/role';
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
@@ -11,8 +10,6 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
   const filterStore = useFilterStore();
   const path: string = to.path;
   const protectedRouteRoles: ERole[] = ProtectedRoute?.[path];
-  const toast = useToast();
-  const { $i18n } = useNuxtApp();
   const isNonLoggedInRoute = NonLoggedInRoutes.includes(path);
   let authorizationCode;
   const router = useRouter();
@@ -49,12 +46,10 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
       userStore.user?.role &&
       !protectedRouteRoles.includes(userStore.user?.role)
     ) {
-      toast.error($i18n.t('server_error_messages.WRONG_ROLE'));
       return router.push({ path: '/' });
     }
 
     if (authorizationCode !== 200) {
-      toast.error($i18n.t('server_error_messages.UNAUTHORIZED'));
       return router.push({
         path: '/login',
         query: { next: to.fullPath.slice(1) },
@@ -65,8 +60,6 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
   if (isNonLoggedInRoute) {
     if (authorizationCode === 200 && userStore.user?.id) {
       const role = userStore.user?.role;
-
-      toast.error($i18n.t('server_error_messages.LOGGED_IN_USER_NOT_ALLOWED'));
 
       if (role === ERole.CLIENT) {
         return router.push({ path: '/profile' });
